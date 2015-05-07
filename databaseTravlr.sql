d572b578e3ee8597da1ff902fb644cb60581bef7
AndyPala <andres_go5@hotmail.com>
2015-05-06 22:54:30 -0500

    Correcciones
    Correcciones de sintaxis (sql)

--- databaseTravlr.sql
+++ databaseTravlr.sql
@@ -1,68 +1,67 @@
-DROP DATABASE IF EXISTS DBName;
-CREATE DATABASE DBName;
-USE DBName;
+DROP DATABASE IF EXISTS TravelrDB;
+CREATE DATABASE TravelrDB;
+USE TravelrDB;
 
 DROP TABLE IF EXISTS Review;
+DROP TABLE IF EXISTS Place;
 DROP TABLE IF EXISTS Category;
 DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS Place;
 
-CREATE TABLE User(
-	IdUser UNSIGNED INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	Username VARCHAR(16) NOT NULL,
-	PasswordUser VARCHAR(16) NOT NULL,
-	EmailUser VARCHAR(50),
-	ActivoUser TINYINT UNSIGNED NOT NULL
+CREATE TABLE User (
+    IdUser INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(16) NOT NULL,
+    PasswordUser VARCHAR(16) NOT NULL,
+    EmailUser VARCHAR(50),
+    ActivoUser TINYINT UNSIGNED NOT NULL
 );
 
 CREATE TABLE Category(
-	idCategory UNSIGNED INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	descCategory TEXT
+	idCategory INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	descCategory TEXT NOT NULL
 );
 
 CREATE TABLE Place(
-	IdPlace UNSIGNED INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	idCategory INT,
+	IdPlace INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	idCategory INT UNSIGNED,
 	NamePlace VARCHAR(50) NOT NULL,
 	LatitudePlace FLOAT(10, 6) NOT NULL,
 	LongitudePlace FLOAT(10, 6) NOT NULL,
 
-	FOREING KEY (idCategory) REFERENCES Category(idCategory)
+	foreign key(idCategory) REFERENCES Category(idCategory)
 );
 
 CREATE TABLE Review(
-	IdReview UNSIGNED INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	IdUser INT,
-	IdPlace INT,
+	IdReview INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	IdUser INT UNSIGNED,
+	IdPlace INT UNSIGNED,
 	ReviewDesc TEXT NOT NULL,
-	ReviewStars TINYINT UNSIGNED,
-
-	FOREING KEY (IdUser) REFERENCES User(IdUser),
-	FOREING KEY (IdPlace) REFERENCES Place(IdPlace)
+	ReviewStars TINYINT UNSIGNED NOT NULL,
+    
+	foreign key(IdUser) REFERENCES User(IdUser),
+	foreign key(IdPlace) REFERENCES Place(IdPlace)
 );
 
 DELIMITER //
-
-DROP PROCEDURE IF EXISTS sp_insertar_usuario
+DROP PROCEDURE IF EXISTS sp_insertar_usuario//
 CREATE PROCEDURE sp_insertar_usuario(IN nameParam VARCHAR(16), IN emailParam VARCHAR(50), IN passParam VARCHAR(16))
 BEGIN
 
 	declare cont int;
 	set cont = (
 		SELECT COUNT(*)
-		FROM 'DBName'.'User'
+		FROM TravelrDB.User
 		WHERE
-			'DBName'.'Username' = nameParam
-			OR 'DBName'.'EmailUser' = emailParam);
+			User.Username = nameParam
+			OR User.EmailUser = emailParam);
 
 	if cont > 0 then
-		SELECT 0 as Valid
+		SELECT 0 as Valid;
 	else
-		INSERT INTO 'DBName'.'User'
-			('Username',
-			'PasswordUser',
-			'EmailUser',
-			'ActivoUser')
+		INSERT INTO TravelrDB.User(
+			Username,
+			PasswordUser,
+			EmailUser,
+			ActivoUser)
 		VALUES
 			(nameParam,
 			passParam,
@@ -74,17 +73,16 @@ BEGIN
 	end if;
 END//
 
-DROP PROCEDURE IF EXISTS sp_login_user
+DELIMITER //
+DROP PROCEDURE IF EXISTS sp_login_user//
 CREATE PROCEDURE sp_login_user(IN  nameParam VARCHAR(16), IN passParam VARCHAR(16))
 BEGIN
 	SELECT
 		COUNT(*) as Valid,
-		'User'.'Username',
-		'User'.'PasswordUser'
-	FROM 'DBName'.'User'
+		User.Username,
+		User.PasswordUser
+	FROM TravelrDB.User
 	WHERE
-		'DBName'.'Username' = nameParam
-		AND 'DBName'.'PasswordUser' = passParam;
+		User.Username = nameParam
+		AND User.PasswordUser = passParam;
 END//
-
-
